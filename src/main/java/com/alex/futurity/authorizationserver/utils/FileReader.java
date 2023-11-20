22d18c2772857cfa222961453dc46461d558c751
ThePersonThat <gaudabrus@gmail.com>
2023-11-20 23:00:48 +0200

    Read file fix

--- src/main/java/com/alex/futurity/authorizationserver/utils/FileReader.java
+++ src/main/java/com/alex/futurity/authorizationserver/utils/FileReader.java
@@ -2,19 +2,17 @@ package com.alex.futurity.authorizationserver.utils;
 
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 @Slf4j
 @UtilityClass
 public class FileReader {
     public static String readFileToString(String path) {
         try {
-            File file = ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + path);
-            return new String(Files.readAllBytes(file.toPath()));
+            byte[] bytes = new ClassPathResource(path).getInputStream().readAllBytes();
+            return new String(bytes);
         } catch (IOException e) {
             throw new IllegalStateException(
                     String.format("Error loading \"%s\" file: " + e.getMessage(), path)
